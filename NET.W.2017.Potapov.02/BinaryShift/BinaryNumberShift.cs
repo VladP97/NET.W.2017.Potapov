31e1fe366290139b3d32bf550af18b29f0038f5f
VladP97 <vlad.potapov1997@gmail.com>
2017-10-23 17:08:06 +0300

    NUnit tests replaced, NET.W.2017.Potapov.02(NUnit) deleted

--- NET.W.2017.Potapov.02/BinaryShift/BinaryNumberShift.cs
+++ NET.W.2017.Potapov.02/BinaryShift/BinaryNumberShift.cs
@@ -8,43 +8,18 @@ namespace BinaryShift
 {
 	public static class BinaryNumberShift
 	{
-		private static string insertBits(this string numberToInsert)
-		{
-			string str = new string('0', 31 - numberToInsert.Length);
-			return str + numberToInsert;
-		}
-
 		public static int InsertNumber(int numberSource, int numberToInsert, int shift, int shiftTo)
 		{
-			string binNumberSource = Convert.ToString(numberSource, 2);
 			string binNumberToInsert = Convert.ToString(numberToInsert, 2);
-			char[] binNumberSourceCharArray = binNumberSource.insertBits().ToCharArray();
-			char[] binNumberToInsertCharArray = binNumberToInsert.insertBits().ToCharArray();
-			Array.Reverse(binNumberSourceCharArray);
-			Array.Reverse(binNumberToInsertCharArray);
-			int j = shift;
-			for (int i = 0; i <= shiftTo - shift; i++)
+			int bitsToAdd = shiftTo - shift + 1;
+			char[] binNumberToInsertCharArray = binNumberToInsert.ToCharArray();
+			if (bitsToAdd < binNumberToInsertCharArray.Length)
 			{
-				if (binNumberSourceCharArray[j] == '1' || binNumberToInsertCharArray[i] == '1')
-				{
-					binNumberSourceCharArray[j] = '1';
-				}
-				else if (binNumberSourceCharArray[j] == '0' && binNumberToInsertCharArray[i] == '0')
-				{
-					binNumberSourceCharArray[j] = '0';
-				}
-				j++;
+				Array.Resize(ref binNumberToInsertCharArray, bitsToAdd);
+				binNumberToInsert = new string(binNumberToInsertCharArray);
+				numberToInsert = Convert.ToInt32(binNumberToInsert, 2);
 			}
-			foreach (var item in binNumberSourceCharArray)
-			{
-				Console.WriteLine(item);
-			}
-			Array.Reverse(binNumberSourceCharArray);
-			int deleteTo = 31 - Array.IndexOf(binNumberSourceCharArray, '1');
-			Array.Reverse(binNumberSourceCharArray);
-			Array.Resize(ref binNumberSourceCharArray, deleteTo);
-			Array.Reverse(binNumberSourceCharArray);
-			return Convert.ToInt32(new string(binNumberSourceCharArray), 2);
+			return numberSource | numberToInsert << shift;
 		}
 	}
 }
