f2c6b9cee5dca883fcad04971c51a9bf07d9e6d3
VladP97 <vlad.potapov1997@gmail.com>
2017-11-26 14:25:03 +0300

    Added mock tests

--- NET.W.2017.Potapov.15/BLL.Tests/BLLTests.cs
+++ NET.W.2017.Potapov.15/BLL.Tests/BLLTests.cs
@@ -15,10 +15,71 @@ namespace BLL.Tests
     public class BLLTests
     {
         [Test]
-        public void AddNewAccountsToService()
+        public void CallAccountNumberCreateService()
         {
+            var mock = new Mock<IAccountNumberCreateService>();
+            var createService = mock.Object;
 
+            createService.GenerateAccountNumber(new List<Account>());
 
+            mock.Verify(cs => cs.GenerateAccountNumber(It.IsAny<IEnumerable<Account>>()));
+        }
+
+        [Test]
+        public void AccountNumberCreateService_ReturnAnyInt()
+        {
+            var mock = new Mock<IAccountNumberCreateService>();
+            var createService = mock.Object;
+
+            createService.GenerateAccountNumber(new List<Account>());
+
+            mock.Setup(cs => cs.GenerateAccountNumber(It.IsAny<IEnumerable<Account>>())).Returns(It.IsAny<int>);
+        }
+
+        [Test]
+        public void AccountService()
+        {
+            var mock = new Mock<IAccountService>();
+            var mockCreator = new Mock<IAccountNumberCreateService>();
+            var mockAccountType = new Mock<AccountType>();
+            var createService = mock.Object;
+
+            createService.OpenAccount("Account owner 1", mockAccountType.Object, mockCreator.Object);
+            createService.OpenAccount("Account owner 2", mockAccountType.Object, mockCreator.Object);
+            createService.OpenAccount("Account owner 3", mockAccountType.Object, mockCreator.Object);
+
+            mock.Verify(cs => cs.OpenAccount(It.IsAny<string>(), mockAccountType.Object, mockCreator.Object), Times.Exactly(3));
+        }
+
+        [Test]
+        public void AccountServiceGetAllAccounts()
+        {
+            var mock = new Mock<IAccountService>();
+            var mockCreator = new Mock<IAccountNumberCreateService>();
+            var mockAccountType = new Mock<AccountType>();
+            var createService = mock.Object;
+
+            createService.OpenAccount("Account owner 1", mockAccountType.Object, mockCreator.Object);
+            createService.OpenAccount("Account owner 2", mockAccountType.Object, mockCreator.Object);
+            createService.OpenAccount("Account owner 3", mockAccountType.Object, mockCreator.Object);
+            createService.GetAllAccounts();
+
+            mock.Verify(cs => cs.GetAllAccounts());
+        }
+
+        [Test]
+        public void AccountServiceGetAllAccountsReturnsIEnumerable()
+        {
+            var mock = new Mock<IAccountService>();
+            var mockCreator = new Mock<IAccountNumberCreateService>();
+            var mockAccountType = new Mock<AccountType>();
+            var createService = mock.Object;
+
+            createService.OpenAccount("Account owner 1", mockAccountType.Object, mockCreator.Object);
+            createService.OpenAccount("Account owner 2", mockAccountType.Object, mockCreator.Object);
+            createService.OpenAccount("Account owner 3", mockAccountType.Object, mockCreator.Object);
+
+            mock.Setup(cs => cs.GetAllAccounts()).Returns(It.IsAny<IEnumerable<Account>>());
         }
     }
 }
