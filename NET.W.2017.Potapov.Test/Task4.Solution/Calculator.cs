5958b4db5ed51d905f0c5af4e94e49094a82abc7
VladP97 <vlad.potapov1997@gmail.com>
2017-11-28 11:13:42 +0300

    Implemented Task4

---
+++ NET.W.2017.Potapov.Test/Task4.Solution/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Solution
+{
+    public class Calculator<T>
+    {
+        List<ICalculate<T>> calculateAlgorithms =  new List<ICalculate<T>>();
+
+        public void Add(ICalculate<T> method)
+        {
+            calculateAlgorithms.Add(method);
+        }
+
+        public ICalculate<T> this[int index]
+        {
+            get { return calculateAlgorithms[index]; }
+        }
+    }
+}
--- NET.W.2017.Potapov.Test/Task4.Tests/TestCalculator.cs
+++ NET.W.2017.Potapov.Test/Task4.Tests/TestCalculator.cs
@@ -1,23 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using NUnit.Framework;
-using Task4;
+using Task4.Solution;
 
 namespace Task4.Tests
 {
+    public class Mean : ICalculate<double>
+    {
+        public double Calculate(List<double> values)
+        {
+            values.Sort();
+
+            int n = values.Count;
+
+            if (n % 2 == 1)
+            {
+                return values[(n - 1) / 2];
+            }
+
+            return (values[n / 2 - 1] + values[n / 2]) / 2;
+        }
+
+        private double[] ToArray(List<double> values)
+        {
+            double[] array = new double[values.Count];
+            int i = 0;
+            foreach (var item in values)
+            {
+                array[i] = item;
+                i++;
+            }
+            return array;
+        } 
+    }
+
+    public class Median : ICalculate<double>
+    {
+        public double Calculate(List<double> values)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+            return Sum(values) / values.Count;
+        }
+
+        private double Sum(List<double> values)
+        {
+            double result = 0;
+            foreach (var item in values)
+            {
+                result += item;
+            }
+
+            return result;
+        }
+    }
+
     [TestFixture]
     public class TestCalculator
     {
         private readonly List<double> values = new List<double> { 10, 5, 7, 15, 13, 12, 8, 7, 4, 2, 9 };
+        Calculator<double> calculator = new Calculator<double>();
 
         [Test]
         public void Test_AverageByMean()
-        {
-            Calculator calculator = new Calculator();
+        {         
+            calculator.Add(new Mean());
 
             double expected = 8.3636363;
 
-            double actual = calculator.CalculateAverage(values, AveragingMethod.Mean);
+            double actual = calculator[0].Calculate(values);
 
             Assert.AreEqual(expected, actual, 0.000001);
         }
@@ -25,11 +78,11 @@ namespace Task4.Tests
         [Test]
         public void Test_AverageByMedian()
         {
-            Calculator calculator = new Calculator();
+            calculator.Add(new Median());
 
-            double expected = 8.0;
+            double expected = 8.3636363;
 
-            double actual = calculator.CalculateAverage(values, AveragingMethod.Median);
+            double actual = calculator[1].Calculate(values);
 
             Assert.AreEqual(expected, actual, 0.000001);
         }
