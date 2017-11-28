6f6cb3f3e579572343e1f7c397207b51273d6db2
VladP97 <vlad.potapov1997@gmail.com>
2017-11-28 08:33:35 +0300

    Test task initial commit

---
+++ NET.W.2017.Potapov.16/XMLTests/UrlXMLFormatterTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML.Tests
+{
+    [TestClass()]
+    public class UrlXMLFormatterTests
+    {
+        [TestMethod()]
+        public void CreateNewXMLFile()
+        {
+            UrlXMLFormatter uxf = new UrlXMLFormatter("test.xml", "https://github.com/AnzhelikaKravchuk?tab=repositories");
+        }
+    }
+}
